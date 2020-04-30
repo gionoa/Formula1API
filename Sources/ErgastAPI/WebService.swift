b3f09f13403b0f66c9630057510663537e27fe30
Giovanni Noa <gio@onramp.io>
2020-04-29 23:09:20 -0700

    Comment out

--- Sources/ErgastAPI/WebService.swift
+++ Sources/ErgastAPI/WebService.swift
@@ -1,64 +1,64 @@
+////
+////  File.swift
+////  
+////
+////  Created by Giovanni Noa on 4/28/20.
+////
 //
-//  File.swift
-//  
+//import Foundation
+//import Combine
 //
-//  Created by Giovanni Noa on 4/28/20.
+//#if canImport(UIKit)
+//extension ErgastAPI {
+//    enum WebService { }
 //
-
-import Foundation
-import Combine
-
-#if canImport(UIKit)
-extension ErgastAPI {
-    enum WebService { }
-
-   
-}
-
-// MARK: - Web Service
-extension WebService {
-       // MARK: - Static functions
-       private static func dataTask(_ url: URL) -> AnyPublisher<Data, Error> {
-           return
-               URLSession.shared
-                   .dataTaskPublisher(for: url)
-                   .tryMap { data, response in
-                       guard
-                           let httpResponse = response as? HTTPURLResponse,
-                           200..<300 ~= httpResponse.statusCode
-                           else {
-                               print(response.description)
-                               throw PublisherError.network
-                       }
-                       return data
-                   }
-                   .eraseToAnyPublisher()
-       }
-       
-       static func fetch<T: Decodable>(_ subPath: Path, for year: Int? = 2019) -> AnyPublisher<T, PublisherError> {
-           let url = Endpoint(subPath, for: year).url!
-           return
-               dataTask(url)
-                   .decode(type: T.self, decoder: JSONDecoder())
-                   .mapError { error in
-                       print(error)
-                       return PublisherError.parsing }
-                   .eraseToAnyPublisher()
-       }
-       
-       static func fetchImage(urlString: String) -> AnyPublisher<UIImage, Error> {
-           let url = URL(string: urlString)
-           return
-               URLSession.shared.dataTaskPublisher(for: url!)
-                   .tryMap { data, response in
-                       guard let image = UIImage(data: data) else {
-                           throw PublisherError.parsing
-                       }
-                       return image
-                   }
-                   .receive(on: RunLoop.main)
-                   .eraseToAnyPublisher()
-          }
-   }
-
-#endif
+//   
+//}
+//
+//// MARK: - Web Service
+//extension WebService {
+//       // MARK: - Static functions
+//       private static func dataTask(_ url: URL) -> AnyPublisher<Data, Error> {
+//           return
+//               URLSession.shared
+//                   .dataTaskPublisher(for: url)
+//                   .tryMap { data, response in
+//                       guard
+//                           let httpResponse = response as? HTTPURLResponse,
+//                           200..<300 ~= httpResponse.statusCode
+//                           else {
+//                               print(response.description)
+//                               throw PublisherError.network
+//                       }
+//                       return data
+//                   }
+//                   .eraseToAnyPublisher()
+//       }
+//       
+//       static func fetch<T: Decodable>(_ subPath: Path, for year: Int? = 2019) -> AnyPublisher<T, PublisherError> {
+//           let url = Endpoint(subPath, for: year).url!
+//           return
+//               dataTask(url)
+//                   .decode(type: T.self, decoder: JSONDecoder())
+//                   .mapError { error in
+//                       print(error)
+//                       return PublisherError.parsing }
+//                   .eraseToAnyPublisher()
+//       }
+//       
+//       static func fetchImage(urlString: String) -> AnyPublisher<UIImage, Error> {
+//           let url = URL(string: urlString)
+//           return
+//               URLSession.shared.dataTaskPublisher(for: url!)
+//                   .tryMap { data, response in
+//                       guard let image = UIImage(data: data) else {
+//                           throw PublisherError.parsing
+//                       }
+//                       return image
+//                   }
+//                   .receive(on: RunLoop.main)
+//                   .eraseToAnyPublisher()
+//          }
+//   }
+//
+//#endif
