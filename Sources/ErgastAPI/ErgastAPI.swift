ad7bfc95c6c2f2da998a3aac8954b3718002fc52
Giovanni Noa <gio@onramp.io>
2020-04-29 22:55:58 -0700

    More testing

--- Sources/ErgastAPI/ErgastAPI.swift
+++ Sources/ErgastAPI/ErgastAPI.swift
@@ -2,10 +2,14 @@ import Foundation
 import CloudKit
 
 struct ErgastAPI {
-   let containerIdentifier = "iCloud.ErgastAPI"
-   let secondContainer = CKContainer(identifier: containerIdentifier)
+    let shared = ErgastAPI()
+    
+    let containerIdentifier = "iCloud.ErgastAPI"
+    let secondContainer = CKContainer(identifier: containerIdentifier)
     
     init() {
         
     }
+    
+    func doSomething()
 }
