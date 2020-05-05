615a421a23c8c30cf9f3963e0041b06270f60560
Giovanni Noa <gio@onramp.io>
2020-05-05 00:30:35 -0700

    Add circuits func for ErgastAPI

--- Sources/ErgastAPI/ErgastAPI.swift
+++ Sources/ErgastAPI/ErgastAPI.swift
@@ -2,20 +2,20 @@ import Foundation
 import CloudKit
 
 public struct ErgastAPI {
-    static let shared = ErgastAPI()
-    
-    let containerIdentifier = "iCloud.ErgastAPI"
+//    static let shared = ErgastAPI()
+//    
+//    let containerIdentifier = "iCloud.ErgastAPI"
    // let secondContainer = CKContainer(identifier: containerIdentifier)
     
-    init() {
-        
-    }
-    
-    public func doSomething() {
-        print(".")
-    }
-    
-    public static func doSomethingButStatic() {
-        print(".")
+    public static func circuits(for season: Season, _ completion: @escaping (Result<[Circuit], ErgastAPIError>) -> Void) {
+        ErgastAPIService.fetch(.circuits, for: .all, decodingType: CircuitsResult.self) { result in
+            switch result {
+            case .success(let response):
+                print(response.circuits)
+                completion(.success(response.circuits))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
 }
