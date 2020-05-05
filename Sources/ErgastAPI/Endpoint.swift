16f3c57f03855e4a69223529abb809848f22d62a
Giovanni Noa <gio@onramp.io>
2020-05-04 18:06:37 -0700

    Add Endpoint struct

--- Sources/ErgastAPI/Endpoint.swift
+++ Sources/ErgastAPI/Endpoint.swift
@@ -1,58 +1,32 @@
 //
-//  File.swift
-//  
+//  Endpoint.swift
+//
 //
 //  Created by Giovanni Noa on 4/28/20.
 //
 
 import Foundation
 
-// MARK: - Path
-enum Path: String {
-    case circuits = "/current/circuits.json"
-    case constructorStandings = "/current/constructorStandings.json"
-    case drivers = "/current/driverStandings.json"
-    case driverStandings = "/driverStandings.json"
-    case results = "/results"
-    case raceStandings = "/current/results.json"
-    case seasons = "/seasons.json"
-     
-    func path(for year: Int = 2019) -> String {
-        switch self {
-        case .circuits:
-            return "/\(year)/circuits.json"
-        case .constructorStandings:
-            return "/\(year)/constructorStandings.json"
-        case .drivers:
-            return "/\(year)/driverStandings.json"
-        case .driverStandings:
-            return "/driverStandings.json"
-        case .results:
-            return "/results.json"
-        case .raceStandings:
-            return "/\(year)/results.json"
-        case .seasons:
-            return "/seasons.json"
-        }
-    }
-}
-
 // MARK: - Endpoint
 struct Endpoint {
-    private (set) var path = "/api/f1"
+    private var path = "/api/f1"
     
-    init(_ subPath: Path, for year: Int? = 2019) {
-        path.append(subPath.path(for: year ?? 2019))
+    init(with path: Path, for season: Season) {
+        self.path.append(path.subPath(for: season))
     }
 }
 
-// MARK: - Path url
+// MARK: - Endpoint URL Builder
 extension Endpoint {
-    var url: URL? {
+    var url: URL {
         var components = URLComponents()
         components.scheme = "https"
         components.host = "ergast.com"
         components.path = path
-        return components.url
+        
+        guard let validURL = components.url else { fatalError() }
+        
+        return validURL
     }
 }
+
