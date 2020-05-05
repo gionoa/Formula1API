2fdbe18ef5f2ca037d4fd29c7343d96fc0651bb0
Giovanni Noa <gio@onramp.io>
2020-05-04 23:13:49 -0700

    Add Endpoint unit tests

---
+++ Tests/ErgastAPITests/EndpointTests.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  
+//
+//  Created by Giovanni Noa on 5/4/20.
+//
+
+import Foundation
+import XCTest
+
+@testable import ErgastAPI
+class EndpointTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func test_emptyTest() {
+        print(">>>>> \(#function) <<<<<")
+    }
+    
+    func test_url_allCircuits_success() {
+        let endpoint = Endpoint(with: .circuits, for: .all)
+        
+        let url = endpoint.url.absoluteString
+        
+        XCTAssertEqual(url, "https://ergast.com/api/f1/circuits.json")
+    }
+    
+    func test_url_yearCircuit_success() {
+        let endpoint = Endpoint(with: .circuits, for: .year(2018))
+        
+        let url = endpoint.url.absoluteString
+        
+        XCTAssertEqual(url, "https://ergast.com/api/f1/2018/circuits.json")
+    }
+}
