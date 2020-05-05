6cb458a46fc44a0e76c9133bbd38fa466e676208
Giovanni Noa <gio@onramp.io>
2020-05-05 00:31:14 -0700

    Add ErgastAPIService class

--- Sources/ErgastAPI/WebService.swift
+++ Sources/ErgastAPI/WebService.swift
@@ -1,64 +1,61 @@
-////
-////  File.swift
-////  
-////
-////  Created by Giovanni Noa on 4/28/20.
-////
 //
-//import Foundation
-//import Combine
+//  File.swift
 //
-//#if canImport(UIKit)
-//extension ErgastAPI {
-//    enum WebService { }
 //
-//   
-//}
+//  Created by Giovanni Noa on 4/28/20.
 //
-//// MARK: - Web Service
-//extension WebService {
-//       // MARK: - Static functions
-//       private static func dataTask(_ url: URL) -> AnyPublisher<Data, Error> {
-//           return
-//               URLSession.shared
-//                   .dataTaskPublisher(for: url)
-//                   .tryMap { data, response in
-//                       guard
-//                           let httpResponse = response as? HTTPURLResponse,
-//                           200..<300 ~= httpResponse.statusCode
-//                           else {
-//                               print(response.description)
-//                               throw PublisherError.network
-//                       }
-//                       return data
-//                   }
-//                   .eraseToAnyPublisher()
-//       }
-//       
-//       static func fetch<T: Decodable>(_ subPath: Path, for year: Int? = 2019) -> AnyPublisher<T, PublisherError> {
-//           let url = Endpoint(subPath, for: year).url!
-//           return
-//               dataTask(url)
-//                   .decode(type: T.self, decoder: JSONDecoder())
-//                   .mapError { error in
-//                       print(error)
-//                       return PublisherError.parsing }
-//                   .eraseToAnyPublisher()
-//       }
-//       
-//       static func fetchImage(urlString: String) -> AnyPublisher<UIImage, Error> {
-//           let url = URL(string: urlString)
-//           return
-//               URLSession.shared.dataTaskPublisher(for: url!)
-//                   .tryMap { data, response in
-//                       guard let image = UIImage(data: data) else {
-//                           throw PublisherError.parsing
-//                       }
-//                       return image
-//                   }
-//                   .receive(on: RunLoop.main)
-//                   .eraseToAnyPublisher()
-//          }
-//   }
-//
-//#endif
+
+import Foundation
+import Combine
+import UIKit
+
+// MARK: - Web Service
+enum ErgastAPIService { }
+
+extension ErgastAPIService
+{
+    // MARK: - Static functions
+    private static func dataTask(_ url: URL,
+                                 _ session: URLSession,
+                                 _ completion: @escaping ((Result<Data, ErgastAPIError>) -> Void))
+    {
+        session.dataTask(with: url) { data, response, error in
+            #warning("Handle the rest of networking")
+            guard let data = data, error == nil else {
+                completion(.failure(.network(error!.localizedDescription)))
+                return
+            }
+            
+            completion(.success(data))
+        }
+        .resume()
+    }
+    
+    static func fetch<T>(_ subPath: Path,
+                                    for season: Season,
+                                    session: URLSession = URLSession.shared,
+                                    decodingType: T.Type,
+                                    _ completion: @escaping ((Result<T, ErgastAPIError>) -> Void)) where T: Decodable {
+        
+        let endpoint = Endpoint(with: subPath, for: season)
+        let url = endpoint.url
+        
+        ErgastAPIService.dataTask(url, session) { result in
+            switch result {
+            case .success(let data) :
+                do {
+                    let decode = try JSONDecoder().decode(T.self, from: data)
+                    
+                    completion(.success(decode))
+                } catch (let error) {
+                    print(error)
+                    completion(.failure(.network(error.localizedDescription)))
+                }
+                
+            case .failure(let error):
+                print(error)
+                fatalError()
+            }
+        }
+    }
+}
