78372d813aafaa568cd07b1640e7b761a8edd257
Giovanni Noa <gio@onramp.io>
2020-05-04 17:50:04 -0700

    Add Season enum

---
+++ Sources/ErgastAPI/Season.swift
@@ -0,0 +1,23 @@
+//
+//  File 2.swift
+//  
+//
+//  Created by Giovanni Noa on 5/4/20.
+//
+
+import Foundation
+
+// MARK: - Season
+enum Season {
+    case all
+    case year(Int)
+    
+    func query() -> String {
+        switch self {
+            case .all:
+            return ""
+            case .year(let year):
+            return "/\(year)"
+        }
+    }
+}
