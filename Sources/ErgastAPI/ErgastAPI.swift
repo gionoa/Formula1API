e392457bd435e48140f97e806d86609d8e9476cf
Giovanni Noa <gio@onramp.io>
2020-05-05 21:41:24 -0700

    Add Ergast API circuit functions that fetch from the respective endpoints

--- Sources/ErgastAPI/ErgastAPI.swift
+++ Sources/ErgastAPI/ErgastAPI.swift
@@ -2,20 +2,39 @@ import Foundation
 import CloudKit
 
 public struct ErgastAPI {
-    static let shared = ErgastAPI()
-    
-    let containerIdentifier = "iCloud.ErgastAPI"
-   // let secondContainer = CKContainer(identifier: containerIdentifier)
-    
-    init() {
-        
+    public static func circuits(for season: Season, _ completion: @escaping (Result<YearCircuits, ErgastAPIError>) -> Void) {
+        ErgastAPIService.fetch(.circuits,
+                               for: season,
+                               decodingType: YearCircuits.self) { result in
+            switch result {
+            case .success(let response):
+                print(response)
+                completion(.success(response))
+                
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
     
-    public func doSomething() {
-        print(".")
+    public static func allCircuits(_ completion: @escaping (Result<AllCircuits, ErgastAPIError>) -> Void) {
+        ErgastAPIService.fetch(.circuits,
+                               for: .all,
+                               decodingType: AllCircuits.self) { result in
+            switch result {
+            case .success(let response):
+                print(response)
+                completion(.success(response))
+                
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
-    
-    public static func doSomethingButStatic() {
-        print(".")
-    }
+//
+//    private static func parseResponse<T: Decodable, U: Error>(_ result: Result<T, U>) -> Result<T, U> {
+//
+//
+//    }
 }
+
