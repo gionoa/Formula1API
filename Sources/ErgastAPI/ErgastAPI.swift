351f5977313170dea8573b09ff942aa31f5f90de
Giovanni Noa <self@gionoa.dev>
2020-05-10 18:15:18 -0700

    Add constructors endpoints

--- Sources/ErgastAPI/ErgastAPI.swift
+++ Sources/ErgastAPI/ErgastAPI.swift
@@ -36,5 +36,27 @@ public struct ErgastAPI {
             completion(result)
         }
     }
+    
+    /// Fetches Formula 1 Constructors for all seasons throughout history.
+    /// - Parameter completion: Asynchronous closure to inject functionality once the network interaction completes.
+    public static func allConstructors<T: Decodable>(completion: @escaping (Result<T, ErgastAPIError>) -> Void) {
+        
+        URLSession.shared.fetch(.constructors) { result in
+            completion(result)
+        }
+    }
+    
+    /// Fetches Formula 1 Constructors for all seasons throughout history.
+    /// - Parameters:
+    ///   - season: Season enum case, specified by an Int, which indicates to fetch data for a given year (1950-2020).
+    ///   - completion: Asynchronous closure to inject functionality once the network interaction completes.
+    public static func constructors<T: Decodable>(for season: SeasonYear,
+                                                  completion: @escaping (Result<T, ErgastAPIError>) -> Void) {
+        
+        URLSession.shared.fetch(.constructors,
+                                for: season) { result in
+            completion(result)
+        }
+    }
 }
 
