7ada55048439fe044952c004284c16485b3c7494
Giovanni Noa <self@gionoa.dev>
2020-05-10 22:30:16 -0700

    Add race schedule decodable structs

---
+++ Sources/ErgastAPI/Models/RaceSchedule.swift
@@ -0,0 +1,58 @@
+//
+//  RaceSchedule.swift
+//  
+//
+//  Created by Giovanni Noa on 5/10/20.
+//
+
+import Foundation
+
+struct RaceSchedule: Codable {
+    let data: RaceScheduleData
+
+    enum CodingKeys: String, CodingKey {
+        case data = "MRData"
+    }
+}
+
+struct RaceScheduleData: Codable {
+    let series: String
+    let url: String
+    let limit: String
+    let offset: String
+    let total: String
+    let raceTable: RaceTable
+
+    enum CodingKeys: String, CodingKey {
+        case series
+        case url
+        case limit
+        case offset
+        case total
+        case raceTable = "RaceTable"
+    }
+}
+
+struct RaceTable: Codable {
+    let season: String
+    let races: [Race]
+
+    enum CodingKeys: String, CodingKey {
+        case season
+        case races = "Races"
+    }
+}
+
+struct Race: Codable {
+    let season, round: String
+    let url: String
+    let raceName: String
+    let circuit: Circuit
+    let date, time: String
+
+    enum CodingKeys: String, CodingKey {
+        case season, round, url, raceName
+        case circuit = "Circuit"
+        case date, time
+    }
+}
