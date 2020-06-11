de0d168ef00b8df6e6a69fecad1ca6479b9c9c6e
Giovanni Noa <self@gionoa.dev>
2020-06-11 00:07:03 -0700

    Add pit stops path enum and support

--- Sources/ErgastAPI/Helpers/Path.swift
+++ Sources/ErgastAPI/Helpers/Path.swift
@@ -25,6 +25,9 @@ enum Path {
     /// Driver Standings.
     case driverStandings
     
+    /// Pit Stops.
+    case pitStops
+    
     /// Race Schedule.
     case raceSchedule
     
@@ -45,8 +48,9 @@ extension Path {
     /// Function that generates the path for an endpoint within the Ergast API.
     /// - Parameter season: Season enum case, specified by an Int, which indicates to fetch data for a given year (1950-2020).  Data for historical seasons will be fetched if nil.
     /// - Returns: String to be added to the Endpoint path.
-    private func subPath(for season: SeasonYear? = nil) -> String {
+    private func subPath(for season: SeasonYear? = nil, andRound round: String? = nil) -> String {
         let year = season?.query ?? ""
+        let race = round ?? ""
         
         switch self {
         case .circuits:
@@ -59,6 +63,8 @@ extension Path {
             return "\(year)/drivers.json"
         case .driverStandings:
             return "\(year)/driverStandings.json"
+        case .pitStops:
+            return "\(year)/\(race)/pitstops.json"
         case .raceSchedule:
             return "\(year).json"
         case .raceResults:
@@ -75,8 +81,10 @@ extension Path {
     /// Constructs a path.
     /// - Parameter season: Season enum case, specified by an Int, which indicates to fetch data for a given year (1950-2020). All historical seasons will be fetched if nil.
     /// - Returns: String representing a URL path.
-    func urlPath(for season: SeasonYear?) -> String {
-        return Path.basePath + subPath(for: season)
+    func urlPath(for season: SeasonYear?,
+                 round: String?) -> String {
+        return Path.basePath + subPath(for: season,
+                                       andRound: round)
     }
     
     /// Returns a Decodable type for a given endpoint.
